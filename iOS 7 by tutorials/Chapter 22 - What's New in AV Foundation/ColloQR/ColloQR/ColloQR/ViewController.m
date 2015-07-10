f23e1e71a9f6a4e3d0e537638bcab6c6c4a0e2dc
lanjing99 <84336951@qq.com>
2015-07-10 15:29:50 +0800

    ColloQR add video session

--- iOS 7 by tutorials/Chapter 22 - What's New in AV Foundation/ColloQR/ColloQR/ColloQR/ViewController.m
+++ iOS 7 by tutorials/Chapter 22 - What's New in AV Foundation/ColloQR/ColloQR/ColloQR/ViewController.m
@@ -7,16 +7,84 @@
 //
 
 #import "ViewController.h"
+@import AVFoundation;
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+    AVCaptureSession *_captureSession;
+    AVCaptureDevice *_videoDevice;
+    AVCaptureDeviceInput *_videoInput;
+    AVCaptureVideoPreviewLayer *_previewLayer;
+    BOOL _running;
+}
+
+
+-(void)setupCaptureSession
+{
+    if(_captureSession) return;
+    _videoDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if(!_videoDevice)
+    {
+        NSLog(@"No video camera on this device!");
+        return;
+    }
+    
+    _videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:_videoDevice error:nil];
+    _captureSession = [[AVCaptureSession alloc] init];
+    if([_captureSession canAddInput:_videoInput])
+    {
+        [_captureSession addInput:_videoInput];
+    }
+    
+    _previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    _previewLayer.videoGravity = AVLayerVideoGravityResize;
+}
+
+
+    
+
+
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupCaptureSession];
+    _previewLayer.frame = self.view.bounds;
+    [self.view.layer addSublayer:_previewLayer];
+}
+
+-(void)startRunning
+{
+    if(_running) return;
+    [_captureSession startRunning];
+    _running = YES;
+}
+
+-(void)stopRunning
+{
+    if(!_running) return;
+    [_captureSession stopRunning];
+    _running = NO;
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self startRunning];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self stopRunning];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,3 +93,13 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
