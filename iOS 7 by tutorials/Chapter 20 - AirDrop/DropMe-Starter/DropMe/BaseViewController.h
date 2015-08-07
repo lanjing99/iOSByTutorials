c6001a0ee710f328b81ce3ced580df00eedea770
lanjing99 <84336951@qq.com>
2015-08-08 07:27:12 +0800

    add string airdrop share

---
+++ iOS 7 by tutorials/Chapter 20 - AirDrop/DropMe-Starter/DropMe/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  DropMe
+//
+//  Created by lanjing on 15/8/8.
+//  Copyright (c) 2015年 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
