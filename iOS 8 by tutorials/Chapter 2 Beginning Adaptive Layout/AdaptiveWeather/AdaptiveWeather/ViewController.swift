5f373cadf1b3b85d755571c2f6400335e92b1168
lanjing <84336951@qq.com>
2016-02-25 09:38:21 +0800

    AdaptiveWeather

---
+++ iOS 8 by tutorials/Chapter 2 Beginning Adaptive Layout/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by lanjing on 2/25/16.
+//  Copyright © 2016 lanjing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
