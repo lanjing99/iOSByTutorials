5b5467d355fc5f7e91e4948bf60524e5fdae4693
lanjing <84336951@qq.com>
2016-03-09 10:02:58 +0800

    add address textfield

--- iOS 8 by tutorials/Chapter 24 - Beginning WebKit/WebKitStarter/WebKitStarter/ViewController.swift
+++ iOS 8 by tutorials/Chapter 24 - Beginning WebKit/WebKitStarter/WebKitStarter/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var inputURLTextfield: UITextField!
     @IBOutlet weak var barBackgroundView: UIView!
+    
     var webView: WKWebView
+    
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,13 +36,14 @@ class ViewController: UIViewController {
         let heightConstraint = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0)
         view.addConstraint(heightConstraint)
         
-        let verticalContrains = NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
-        view.addConstraints(verticalContrains)
-        let horizontalContrains = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
-        view.addConstraints(horizontalContrains)
+//        let verticalContrains = NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
+//        view.addConstraints(verticalContrains)
+//        let horizontalContrains = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
+//        view.addConstraints(horizontalContrains)
      
         
-        let URL = NSURL(string: "http://www.baidu.com")
+        inputURLTextfield.text = "http://www.baidu.com"
+        let URL = NSURL(string: inputURLTextfield.text!)
         let request = NSURLRequest(URL:URL!)
         webView.loadRequest(request)
     }
@@ -51,6 +55,27 @@ class ViewController: UIViewController {
         barBackgroundView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
     }
 
+    func cancel () {
+        inputURLTextfield.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+        barBackgroundView.frame = CGRect(x:0, y: 0, width:view.frame.width, height: 30)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        let cancelButton = UIBarButtonItem(title: "Cancel", style:.Plain, target: self, action: "cancel")
+        navigationItem.rightBarButtonItem = cancelButton
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        inputURLTextfield.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+        barBackgroundView.frame = CGRect(x:0, y: 0, width:view.frame.width, height: 30)
+        if let string = inputURLTextfield.text {
+            if let URL = NSURL(string:string){
+                webView.loadRequest(NSURLRequest(URL: URL))
+            }
+        }
+        return false
+    }
 }
 
