aa9bdd6ff38320cb96a000ee269ecfd5027bc565
lanjing <84336951@qq.com>
2016-03-09 09:39:27 +0800

    add a simple WebView

--- iOS 8 by tutorials/Chapter 24 - Beginning WebKit/WebKitStarter/WebKitStarter/ViewController.swift
+++ iOS 8 by tutorials/Chapter 24 - Beginning WebKit/WebKitStarter/WebKitStarter/ViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barBackgroundView: UIView!
+    var webView: WKWebView
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        webView = WKWebView(frame: CGRectZero)
+        webView.backgroundColor = UIColor.blueColor()
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        barBackgroundView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let widthConstraint = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
+        view.addConstraint(widthConstraint)
+        let heightConstraint = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0)
+        view.addConstraint(heightConstraint)
+        
+        let verticalContrains = NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
+        view.addConstraints(verticalContrains)
+        let horizontalContrains = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[webView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["webView" : webView])
+        view.addConstraints(horizontalContrains)
+     
+        
+        let URL = NSURL(string: "http://www.baidu.com")
+        let request = NSURLRequest(URL:URL!)
+        webView.loadRequest(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        barBackgroundView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 30)
     }
 
 
