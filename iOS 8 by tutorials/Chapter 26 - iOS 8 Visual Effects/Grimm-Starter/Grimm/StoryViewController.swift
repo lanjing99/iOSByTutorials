4802eb9a9ff0cea8b7dd575e825ea3c96f9c7926
lanjing <84336951@qq.com>
2016-03-10 10:43:24 +0800

    一个奇怪的错误，进入断点调试就不会崩溃，没进入断点就会崩溃

--- iOS 8 by tutorials/Chapter 26 - iOS 8 Visual Effects/Grimm-Starter/Grimm/StoryViewController.swift
+++ iOS 8 by tutorials/Chapter 26 - iOS 8 Visual Effects/Grimm-Starter/Grimm/StoryViewController.swift
@@ -28,7 +28,9 @@ class StoryViewController: UIViewController, ThemeAdopting {
   @IBOutlet weak var storyView: StoryView!
   @IBOutlet weak var optionsContainerView: UIView!
   @IBOutlet weak var optionsContainerViewBottomConstraint: NSLayoutConstraint!
+  
   var showingOptions = false
+  var blurView = UIImageView()
   
   var story: Story?
   
@@ -52,6 +54,25 @@ class StoryViewController: UIViewController, ThemeAdopting {
     if story != nil {
       storyView.story = story
     }
+//    optionsContainerView.subviews[0].insertSubview(blurView, atIndex: 0)
+    let blurEffect = UIBlurEffect(style: .Light)
+    let blurView = UIVisualEffectView(effect: blurEffect)
+    blurView.translatesAutoresizingMaskIntoConstraints = false
+    view.insertSubview(blurView, atIndex: 0)
+    
+    view.addConstraint(NSLayoutConstraint(item: blurView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0))
+    view.addConstraint(NSLayoutConstraint(item: blurView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0))
+    
+    // 1
+    let vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect) // 2
+    let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+    vibrancyView.translatesAutoresizingMaskIntoConstraints = false // 3
+    vibrancyView.contentView.addSubview(optionsContainerView) // 4
+    blurView.contentView.addSubview(vibrancyView)
+//
+    view.addConstraint(NSLayoutConstraint(item: vibrancyView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height,multiplier: 1, constant: 0))
+    view.addConstraint(NSLayoutConstraint(item: vibrancyView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width,
+      multiplier: 1, constant: 0))
   }
   
   override func viewWillAppear(animated: Bool) {
@@ -64,6 +85,9 @@ class StoryViewController: UIViewController, ThemeAdopting {
   }
   
   private func setOptionsHidden(hidden: Bool, animated: Bool) {
+    if !hidden {
+//      updateBlur()
+    }
     showingOptions = !hidden;
     let height = CGRectGetHeight(optionsContainerView.bounds)
     var constant = optionsContainerViewBottomConstraint.constant
@@ -99,4 +123,37 @@ class StoryViewController: UIViewController, ThemeAdopting {
     }
   }
   
+  func updateBlur(){
+    optionsContainerView.hidden = true
+    UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, true, 1)
+    self.view.drawViewHierarchyInRect(self.view.bounds, afterScreenUpdates: true)
+    let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    let blur = screenshot.applyLightEffect()
+    blurView.frame = optionsContainerView.bounds
+    blurView.image = blur
+//    blurView.contentMode = .Bottom
+    optionsContainerView.hidden = false
+    
+  }
+  
+  override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    coordinator.animateAlongsideTransition(nil) { (context) -> Void in
+      self.updateBlur()
+    }
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
